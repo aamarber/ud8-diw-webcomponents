ea1863bf62f816cfcfa2495d17f18d1281aca01a
melocotoneroh <aamarber@gmail.com>
2023-02-06 14:59:10 +0100

    Initial state

---
+++ navbar/navbar.js
@@ -0,0 +1,54 @@
+(function(){
+    class NavbarComponent extends HTMLElement{
+        constructor(){
+            super();
+
+            // attaches shadow tree and returns shadow root reference
+            // https://developer.mozilla.org/en-US/docs/Web/API/Element/attachShadow
+            const shadow = this.attachShadow({ mode: 'open' });
+
+            const navbarElement = this.createNavbar();
+
+            // appending the container to the shadow DOM
+            shadow.appendChild(navbarElement);
+        }
+
+        createNavbar(){
+            let navbarContainer = document.createElement('nav');
+
+            navbarContainer.className = 'navbar navbar-expand-sm';
+
+            navbarContainer.innerHTML = `
+            <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    ${this.navItems.map(navItem => `<nav-item link='${navItem}' display-text='${navItem}'/>`)}
+                </ul>
+                </div>
+            </div>
+            `;
+
+            return navbarContainer;
+        }
+
+        get navItems(){
+            let items = [];
+
+            [...this.attributes].forEach(attr => {
+                if (attr.name.includes('nav-item')) {
+                    items.push(attr.value);
+                }
+            });
+
+            return items;
+        }
+    }
+
+    // let the browser know about the custom element
+    customElements.define('navbar-component', NavbarComponent);
+})();
---
+++ navbar/navbarItem.js
@@ -0,0 +1,46 @@
+(function(){
+    class NavBarItem extends HTMLElement{
+        constructor(){
+            super();
+
+            // attaches shadow tree and returns shadow root reference
+            // https://developer.mozilla.org/en-US/docs/Web/API/Element/attachShadow
+            const shadow = this.attachShadow({ mode: 'open' });
+
+            const bootstrapStyles = this.buildBootstrapStylesReference();
+
+            const navbarItem = this.buildNavBarItem();
+
+            shadow.appendChild(bootstrapStyles);
+            shadow.appendChild(navbarItem);
+        }
+
+        buildBootstrapStylesReference(){
+            let bootstrapStyles = document.createElement('link');
+
+            bootstrapStyles.rel = 'stylesheet';
+            bootstrapStyles.href = './node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+            return bootstrapStyles;
+        }
+
+        buildNavBarItem(){
+            let navbarListItem = document.createElement('li');
+
+            navbarListItem.className = 'nav-item';
+
+            navbarListItem.innerHTML = `
+            <a class="nav-link" href=${this.link}>${this.link}</a>
+            `;
+
+            return navbarListItem;
+        }
+
+        get link(){
+            return this.attributes.getNamedItem('link')?.value;
+        }
+    }
+
+    // let the browser know about the custom element
+    customElements.define('nav-item', NavBarItem);
+})();
